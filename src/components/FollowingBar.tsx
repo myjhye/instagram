4e804c5f090ec94d0f9d04b5403eb3e5ed246a0b
myjhye <t01087633799@gmail.com>
2023-12-02 23:52:08 +0900

    팔로잉바 -> swr 적용

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,4 +1,17 @@
+'use client';
+
+import useSWR from 'swr';
+
 export default function FollowingBar() {
+
+    const { data, isLoading, error } = useSWR('/api/me');
+    
+    console.log(data);
+
+    // 1. 클라이언트 컴포넌트에서 -> 백엔드에게 api/me 사용자의 정보 가져오기
+    // 2. 백엔드에서 현재 로그인된 사용자의 세션 정보를 이용하여
+    // 3. 벡엔드에서 followings의 상세 정보를 sanity에서 가져오기
+    // 4. 클라이언트 컴포넌트에서 followings 정보를 화면에 표시 -> image, username
 
     return (
         <p>following bar</p>
