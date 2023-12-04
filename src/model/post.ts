7b95c91833ce0e5b9668bc8d44dcf64959549579
myjhye <t01087633799@gmail.com>
2023-12-05 01:32:28 +0900

    오타 수정

--- src/model/post.ts
+++ src/model/post.ts
@@ -4,7 +4,7 @@ export type Comment = {
     image: string;
 };
 
-export type SimplePost = Omit<FullPost, 'commnets'> & {
+export type SimplePost = Omit<FullPost, 'comments'> & {
     comments: number;
 }
 
