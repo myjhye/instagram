fcab0db243bc864e74cd97736a2549e0f885dbb4
myjhye <t01087633799@gmail.com>
2023-12-04 22:52:29 +0900

    팔로잉바 -> 라이브러리

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { PropagateLoader } from 'react-spinners';
 import useSWR from 'swr';
 import Avatar from './Avatar';
+import ScrollableBar from './ui/ScrollableBar';
 
 export default function FollowingBar() {
 
@@ -26,19 +27,20 @@ export default function FollowingBar() {
             }
             {
                 users && users.length > 0 && (
-                    <ul className='w-full flex gap-2'>
+                    <ScrollableBar>
                         { users.map((user) => (
-                            <li key={user.username}>
-                                <Link
-                                    className='flex flex-col items-center w-20' 
-                                    href={`/user/${user.username}`}
-                                >
-                                    <Avatar image={user.image} highlight />
-                                    <p className='w-full text-sm text-center text-ellipsis overflow-hidden'>{user.username}</p>
-                                </Link>
-                            </li>
+                            <Link
+                                key={user.username}
+                                className='flex flex-col items-center w-20' 
+                                href={`/user/${user.username}`}
+                            >
+                                <Avatar image={user.image} highlight />
+                                <p className='w-full text-sm text-center text-ellipsis overflow-hidden'>
+                                    {user.username}
+                                </p>
+                            </Link>
                         ))}
-                    </ul>
+                    </ScrollableBar>
                 )
             }
         </section>
