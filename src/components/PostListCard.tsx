1a3ff92dfe49be2dfcefd5a7abf4540b1d0e8e5d
myjhye <t01087633799@gmail.com>
2023-12-07 21:23:35 +0900

    포스트 목록 -> 리팩토링

--- src/components/PostListCard.tsx
+++ src/components/PostListCard.tsx
@@ -1,16 +1,15 @@
 import { SimplePost } from "@/model/post"
 import Avatar from "./Avatar";
 import Image from "next/image";
-import HeartIcon from "./ui/icons/HeartIcon";
-import BookmarkIcon from "./ui/icons/BookmarkIcon";
-import { parseDate } from "@/util/date";
-import SmileIcon from "./ui/icons/SmileIcon";
+import CommentForm from "./CommentForm";
+import ActionBar from "./ActionBar";
 
 type Props = {
     post: SimplePost;
+    priority?: boolean;
 }
 
-export default function PostListCard({post}: Props) {
+export default function PostListCard({post, priority = false }: Props) {
 
     const {userImage, username, image, createdAt, likes, text} = post;
 
@@ -22,7 +21,7 @@ export default function PostListCard({post}: Props) {
                     size='medium'
                     highlight 
                 />
-                <span className="text-gray-900 font-bold ml-2">{username}</span>
+                <span className="text-gray-900 font-bold ml-2 font-sans text-base">{username}</span>
             </div>
             <Image
                 className="w-full object-cover ascpect-square" 
@@ -30,30 +29,15 @@ export default function PostListCard({post}: Props) {
                 alt={`photo by ${username}`}
                 width={500}
                 height={500}
+                priority={priority}
             />
-            <div className="flex justify-between my-2 px-4">
-                <HeartIcon />
-                <BookmarkIcon />
-            </div>
-            <div className="px-4 py-1">
-                <p className="text-sm font-bold mb-2">{`${likes?.length ?? 0} ${likes?.length > 1 ? 'likes' : 'like'}`}</p>
-                <p>
-                    <span className="font-bold mr-1">{username}</span>
-                    {text}
-                </p>
-                <p className="text-xs text-neutral-500 uppercase my-2">
-                    {parseDate(createdAt)}
-                </p>
-                <form className="flex items-center border-t border-neutral-300">
-                    <SmileIcon />
-                    <input 
-                        className="w-full ml-2 border-none outline-none p-3"
-                        type='text' 
-                        placeholder="댓글 달기..." 
-                    />
-                    <button className="font-bold text-sky-500 ml-2">Post</button>
-                </form>
-            </div>
+            <ActionBar
+                likes={likes}
+                username={username}
+                text={text}
+                createdAt={createdAt}
+            />
+            <CommentForm />
         </article>
     )
 }
