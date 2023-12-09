ba8a62f7f4b408ed43a17f4452c0d01f6c9e466a
myjhye <t01087633799@gmail.com>
2023-12-09 15:12:31 +0900

    사용자 검색 -> 컴포넌트

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,22 +1,45 @@
 'use client';
 
 import useSWR from 'swr';
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import { ProfileUser } from '@/model/user';
+import GridSpinner from './ui/GridSpinner';
 
 export default function UserSearch() {
 
     const [keyword, setKeyword] = useState('winter');
     const {
-        data,
+        data: users,
         isLoading,
         error
-    } = useSWR(`/api/search/${keyword}`)
+    } = useSWR<ProfileUser[]>(`/api/search/${keyword}`)
 
-    console.log(data);
+    const onSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+    };
 
     return (
         <>
-        
+            <form onSubmit={onSubmit}>
+                <input 
+                    type='text' 
+                    autoFocus
+                    placeholder='유저 이름이나 이름으로 검색'
+                    value={keyword}
+                    onChange={(e) => setKeyword(e.target.value)}
+                />
+            </form>
+            {error && <p>에러 발생</p>}
+            {isLoading && <GridSpinner />}
+            {!isLoading && !error && users?.length === 0 && <p>찾는 사용자가 없음</p>}
+            <ul>
+                {users && users.map(user => (
+                    <li key={user.username}>
+                        <p>{user.username}</p>
+                    </li>   
+                ))}
+            </ul>
         </>
     )
 }
