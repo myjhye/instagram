eba469a72cc39a07de22095d7e8ae2451441f8e2
myjhye <t01087633799@gmail.com>
2023-12-09 21:36:29 +0900

    리팩토링 -> 타입 이름

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,19 +1,20 @@
-export type User = {
+export type AuthUser = {
     name: string;
     username: string;
     email: string;
     image?: string;
 }
 
-export type SimpleUser = Pick<User, 'username' | 'image'>;
+export type SimpleUser = Pick<AuthUser, 'username' | 'image'>;
 
-export type DetailUser = User & {
+export type HomeUser = AuthUser & {
     following: SimpleUser[];
     followers: SimpleUser[];
     bookmarks: string[];
 };
 
-export type ProfileUser = User & {
+export type SearchUser = AuthUser & {
     following: number;
     followers: number;
-}
+};
+
