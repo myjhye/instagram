b31ebaa2649b7550f1369bf8582c012ca65743bd
myjhye <t01087633799@gmail.com>
2023-12-10 09:54:51 +0900

    name -> username

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -56,7 +56,7 @@ export default function Navbar() {
                     }
                     {user && (
                         <li>
-                            <Link href={`/user/${user.name}` }>
+                            <Link href={`/user/${user.username}` }>
                                 <Avatar 
                                     image={user.image}
                                     size='small'
