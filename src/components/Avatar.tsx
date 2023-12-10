5d5f22c04746708862829ea8f79674ab83d14838
myjhye <t01087633799@gmail.com>
2023-12-10 12:10:34 +0900

    사용자 프로필 -> 스타일링

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,4 +1,4 @@
-type AvatarSize = 'small' | 'medium' | 'large';
+type AvatarSize = 'small' | 'medium' | 'large' | 'xlarge';
 
 type Props = {
     image?: string | null
@@ -15,7 +15,7 @@ export default function Avatar({
     return (
         <div className={getContainerStyle(size, highlight)}>
             <img
-                className={`bg-white object-cover rounded-full ${getImageSizeStyle(size)}`}
+                className={`bg-white object-cover rounded-full ${getImageSizeStyle(size).image}`}
                 alt='user profile' 
                 src={image ?? undefined} 
             />
@@ -29,27 +29,52 @@ function getContainerStyle(size: AvatarSize, highlight: boolean): string {
     const highlightStyle = highlight
         ? 'bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300' 
         : '';
-    const sizeStyle = getContainerSize(size); 
-    return `${baseStyle} ${highlightStyle} ${sizeStyle}`
+    const { container } = getImageSizeStyle(size); 
+    return `${baseStyle} ${highlightStyle} ${container}`
 }
 
+type ImageSizeStyle = {
+    container: string;
+    image: string;
+}
 
-function getContainerSize(size: AvatarSize): string {
+function getImageSizeStyle(size: AvatarSize): ImageSizeStyle {
 
     switch(size) {
-        case 'small' : return 'w-9 h-9';
-        case 'medium' : return 'w-11 h-11';
-        case 'large' : return 'w-[68px] h-[68px]';
+        case 'small' : 
+            return { container: 'w-9 h-9', image: 'w-[34px] h-[34px] p-[0.1rem]'};
+        case 'medium' : 
+            return { container: 'w-11 h-11', image: 'w-[42px] h-[42px] p-[0.1rem]'};
+        case 'large' : 
+            return { container: 'w-[68px] h-[68px]', image: 'w-16 h-16 p-[0.2rem]'};
+        case 'xlarge' : 
+            return { container: 'w-[142px] h-[142px]', image: 'w-[138px] h-[138px] p-[0.3rem]'};
+        default :
+            throw new Error(`해당 타입 사이즈를 지원하지 않음: ${size}`);
     }
 }
 
 
+// function getContainerSize(size: AvatarSize): string {
 
-function getImageSizeStyle(size: AvatarSize): string {
+//     switch(size) {
+//         case 'small' : return 'w-9 h-9';
+//         case 'medium' : return 'w-11 h-11';
+//         case 'large' : return 'w-[68px] h-[68px]';
+//         case 'xlarge' : return 'w-[142px] h-[142px]';
+//         default : throw new Error(`해당 타입 사이즈를 지원하지 않음: ${size}`);
+//     }
+// }
 
-    switch(size) {
-        case 'small' : return 'w-[34px] h-[34px] p-[0.1rem]';
-        case 'medium' : return 'w-[42px] h-[42px] p-[0.1rem]';
-        case 'large' : return 'w-16 h-16 p-[0.2rem]';
-    }
-}
+
+
+// function getImageSizeStyle(size: AvatarSize): string {
+
+//     switch(size) {
+//         case 'small' : return 'w-[34px] h-[34px] p-[0.1rem]';
+//         case 'medium' : return 'w-[42px] h-[42px] p-[0.1rem]';
+//         case 'large' : return 'w-16 h-16 p-[0.2rem]';
+//         case 'xlarge' : return 'w-[138px] h-[138px] p-[0.3rem]';
+//         default : throw new Error(`해당 타입 사이즈를 지원하지 않음: ${size}`);
+//     }
+// }
--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -9,30 +9,35 @@ type Props = {
 export default function UserProfile({user}: Props) {
 
     const info = [
-        { title: 'posts', data: user.posts },
-        { title: 'followers', data: user.followers },
-        { title: 'following', data: user.following },
+        { title: '게시물', data: user.posts },
+        { title: '팔로워', data: user.followers },
+        { title: '팔로우', data: user.following },
 
     ]
 
     return (
-        <section>
+        <section className="w-full flex flex-col md:flex-row items-center justify-center py-12 border-b border-neutral-300">
             <Avatar 
                 image={user.image} 
                 highlight
+                size='xlarge'
             />
-            <div>
-                <h1>{user.username}</h1>
-                <FollowButton user={user} />
-                <ul>
+            <div className="md:ml-10 basis-1/3">
+                <div className="flex flex-col items-center md:flex-row">
+                    <h1 className="text-2xl md:mr-8 my-2 md:mb-0">{user.username}</h1>
+                    <FollowButton user={user} />
+                </div>
+                <ul className="my-4 flex gap-4">
                     {info.map((item, index) => (
                         <li key={index}>
-                            <span className="mr-1">{item.data}</span>
                             <span>{item.title}</span>
+                            <span className="font-bold ml-1">{item.data}</span>
                         </li>
                     ))}
                 </ul>
-                <p>{user.name}</p>
+                <p className="text-xl font-bold text-center md:text-start">
+                    {user.name}
+                </p>
             </div>
         </section>
     )
