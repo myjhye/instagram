691ccab0d22b3d4486c7d1f689dcb3938baaa7dc
myjhye <t01087633799@gmail.com>
2023-12-11 21:43:02 +0900

    좋아요 -> 토글 버튼

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -1,6 +1,10 @@
 import HeartIcon from "./ui/icons/HeartIcon";
 import BookmarkIcon from "./ui/icons/BookmarkIcon";
 import { parseDate } from "@/util/date";
+import { useState } from "react";
+import ToggleButton from "./ui/ToggleButton";
+import HeartFilledIcon from "./ui/icons/HeartFilledIcon";
+import BookmarkFilledIcon from "./ui/icons/BookmarkFilledIcon";
 
 type Props = {
     likes: string[];
@@ -11,11 +15,24 @@ type Props = {
 
 export default function ActionBar({likes, username, text, createdAt}: Props) {
 
+    const [liked, setLiked] = useState(false);
+    const [bookmarked, setBookmarked] = useState(false);
+
     return (
         <>
             <div className="flex justify-between my-2 px-4">
-                <HeartIcon />
-                <BookmarkIcon />
+                <ToggleButton 
+                    toggled={liked} 
+                    onToggle={setLiked} 
+                    onIcon={<HeartFilledIcon />}
+                    offIcon={<HeartIcon />} 
+                />
+                <ToggleButton
+                    toggled={bookmarked} 
+                    onToggle={setBookmarked} 
+                    onIcon={<BookmarkFilledIcon />}
+                    offIcon={<BookmarkIcon />} 
+                />
             </div>
             <div className="px-4 py-1">
                 <p className="text-sm font-bold mb-2">{`${likes?.length ?? 0} ${likes?.length > 1 ? 'likes' : 'like'}`}</p>
