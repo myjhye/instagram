d18de766c519aca6cbd0620fce8014001a12fec5
myjhye <t01087633799@gmail.com>
2023-12-15 06:26:19 +0900

    주석 -> 로그인 관련

--- src/app/api/auth/signin/page.tsx
+++ src/app/api/auth/signin/page.tsx
@@ -10,24 +10,33 @@ export const metadata: Metadata = {
     description: '회원가입 또는 로그인',
 }
 
+// 컴포넌트에 전달할 프로퍼티 타입 정의
 type Props = { 
-    searchParams: { 
+    searchParams: {
         callbackUrl: string 
     }
 };
 
 export default async function SignPage({searchParams: { callbackUrl } }: Props) {
 
+    // 서버 세션을 가져와 현재 로그인된 사용자의 세션 확인
     const session = await getServerSession(authOptions);
+
+    // 사용자가 로그인 되어 있으면 홈으로 리다이렉트
     if (session) {
         redirect('/');
     }
 
+    // auth provider 가져오기 -> null이나 undefined면 빈 객체 반환
     const providers = await getProviders() ?? {};
 
     return (
         <section className='flex justify-center mt-[30%]'>
-            <Signin providers={providers} callbackUrl={callbackUrl ?? '/'} />
+            {/* 로그인 컴포넌트에 auth provider 목록, 콜백 url 전달 */}
+            <Signin 
+                providers={providers} 
+                callbackUrl={callbackUrl ?? '/'} 
+            />
         </section>
     );
 }
--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -4,7 +4,9 @@ import { ClientSafeProvider, signIn } from "next-auth/react";
 import ColorButton from "./ui/ColorButton";
 
 type Props = {
+    // auth provider 목록
     providers: Record<string, ClientSafeProvider>;
+    // 로그인 후 리다이렉션 할 url
     callbackUrl: string;
 };
 
@@ -13,11 +15,14 @@ export default function Signin({providers, callbackUrl}: Props) {
     return (
         <>
         {
+            // auth provider 목록 반복하며 로그인 버튼 렌더링
             Object.entries(providers).map(([id, provider]) => (
                 <ColorButton
                     key={id}
                     text={`Sign in with ${provider.name}`}
+                    // 클릭 시 로그인 함수 호출
                     onClick={() => signIn(id, { callbackUrl })}
+                    // 버튼 크기
                     size='big'
                 />
             ))
