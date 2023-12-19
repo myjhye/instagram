84a2539d0e12de0aba526e961fe36c95eb80b03b
myjhye <t01087633799@gmail.com>
2023-12-20 01:45:27 +0900

    .

--- src/app/api/users/[...slug]/route.ts
+++ src/app/api/users/[...slug]/route.ts
@@ -1,39 +1,26 @@
-import { getSavedPostsOf, getPostsOf, getLikedPostsOf } from "@/service/posts";
-import { NextRequest, NextResponse } from "next/server";
+import { getLikedPostsOf, getPostsOf, getSavedPostsOf } from '@/service/posts';
+import { NextRequest, NextResponse } from 'next/server';
 
 type Context = {
-    params: {
-        slug: string[]; 
-    }
-}
-
+  params: {
+    slug: string[];
+  };
+};
 export async function GET(_: NextRequest, context: Context) {
+  const { slug } = context.params;
 
-    const {slug} = context.params;
+  if (!slug || !Array.isArray(slug) || slug.length < 2) {
+    return new NextResponse('Bad Request', { status: 400 });
+  }
 
-    // slug 배열이 없거나, 길이가 2보다 작으면 잘못된 요청으로 처리
-    if (!slug || !Array.isArray(slug) || slug.length < 2) {
-        return new NextResponse('bad request', { status: 400});
-    }
+  const [username, query] = slug;
 
-    // slug 배열의 1, 2번째 요소 추출
-    const [username, query] = slug;
+  let request = getPostsOf;
+  if (query === 'saved') {
+    request = getSavedPostsOf;
+  } else if (query === 'liked') {
+    request = getLikedPostsOf;
+  }
 
-    let request = getPostsOf;
-    
-    // query 값에 따라 요청할 서비스 선택
-
-    // 'saved' 쿼리면 -> 'getSavedPostsOf' 함수 사용
-    if (query === 'saved') {
-        request = getSavedPostsOf;
-    }
-    
-    // 'liked' 쿼리면 -> 'getLikedPostsOf' 함수 사용
-    else if (query === 'liked') {
-        request = getLikedPostsOf;
-    }
-
-    // 선택한 서비스를 이용해 요청 처리 -> 결과를 json 형식으로 반환
-    return request(username)
-        .then((data) => NextResponse.json(data));
+  return request(username).then((data) => NextResponse.json(data));
 }
--- src/service/posts.ts
+++ src/service/posts.ts
@@ -1,3 +1,4 @@
+import post from '../../sanity-studio/schemas/post';
 import { SimplePost } from './../model/post';
 import { client, urlFor } from './sanity';
 
@@ -16,18 +17,15 @@ const simplePostProjection = `
 export async function getFollowingPostsOf(username: string) {
   return client
     .fetch(
-      // 주어진 사용자(username)가 작성한 포스트 & 해당 사용자가 팔로우하는 다른 사용자들의 포스트를 최신 순서로 가져옴 -> sanity query language (groq) 사용
       `*[_type =="post" && author->username == "${username}"
           || author._ref in *[_type == "user" && username == "${username}"].following[]._ref]
           | order(_createdAt desc){
           ${simplePostProjection}
         }`
     )
-    // 데이터 매핑하여 반환
     .then(mapPosts);
 }
 
-// 상세 다일로그 데이터 가져오기 -> 주어진 post id 기반
 export async function getPost(id: string) {
   return client
     .fetch(
@@ -42,18 +40,10 @@ export async function getPost(id: string) {
       "createdAt":_creatdAt
     }`
     )
-    .then((post) => ({ 
-      ...post,
-      // 포스트 이미지를 image url로 변환 -> urlFor 사용 
-      image: urlFor(post.image) 
-    }));
+    .then((post) => ({ ...post, image: urlFor(post.image) }));
 }
 
-// 사용자 게시물 가져오기
-// username -> 게시물을 가져올 사용자명 (username)
 export async function getPostsOf(username: string) {
-
-  // client를 사용해 sanity cms에서 게시물 데이터 가져오기
   return client
     .fetch(
       `*[_type == "post" && author->username == "${username}"]
@@ -61,12 +51,8 @@ export async function getPostsOf(username: string) {
         ${simplePostProjection}
       }`
     )
-    // 가져온 데이터를 가공하는 함수인 mapPosts를 호출해 반환
     .then(mapPosts);
 }
-
-// 사용자가 좋아요 (like)를 누른 게시물 가져오기
-// username -> 게시물을 좋아요한 사용자명 (username)
 export async function getLikedPostsOf(username: string) {
   return client
     .fetch(
@@ -75,12 +61,8 @@ export async function getLikedPostsOf(username: string) {
         ${simplePostProjection}
       }`
     )
-    // 가져온 데이터를 가공하는 함수인 mapPosts를 호출해 반환
     .then(mapPosts);
 }
-
-// 사용자가 저장한 게시물 가져오기
-// username -> 게시물을 저장한 사용자명 (username)
 export async function getSavedPostsOf(username: string) {
   return client
     .fetch(
@@ -89,41 +71,32 @@ export async function getSavedPostsOf(username: string) {
         ${simplePostProjection}
       }`
     )
-    // 가져온 데이터를 가공하는 함수인 mapPosts를 호출해 반환
     .then(mapPosts);
 }
-
-// 포스트 데이터를 가공해 이미지 url 변환하기
-// posts -> 가공할 포스트 데이터 배열
-// 반환 값 -> 이미지 url이 추가된 포스트 데이터 배열 반환
 function mapPosts(posts: SimplePost[]) {
   return posts.map((post: SimplePost) => ({
-    // 기존 포스트 데이터를 유지한 채 복사
     ...post,
     likes: post.likes ?? [],
-    // 이미지 url을 urlFor 함수를 사용해 반환해 추가
     image: urlFor(post.image),
   }));
 }
 
-
-// 포스트 좋아요
 export async function likePost(postId: string, userId: string) {
-
-  return client.patch(postId)
-    .setIfMissing({likes: []})
+  return client
+    .patch(postId) //
+    .setIfMissing({ likes: [] })
     .append('likes', [
       {
         _ref: userId,
-        _type: 'reference'
-      }
+        _type: 'reference',
+      },
     ])
     .commit({ autoGenerateArrayKeys: true });
 }
 
-// 포스트 싫어요
 export async function dislikePost(postId: string, userId: string) {
-  return client.patch(postId)
+  return client
+    .patch(postId)
     .unset([`likes[_ref=="${userId}"]`])
     .commit();
 }
