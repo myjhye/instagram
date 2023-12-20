58b14582bffc0fa5699d491d02d4812d0940216a
myjhye <t01087633799@gmail.com>
2023-12-21 03:57:31 +0900

    팔로우 버튼 -> ui

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -11,20 +11,27 @@ type Props = {
 export default function FollowButton({user}: Props) {
 
     // 현재 로그인된 사용자 정보 가져오기 -> useSWR 사용
-    const { user: loggedInUser } = useMe();
+    const { user: loggedInUser, toggleFollow } = useMe();
+
     // 현재 로그인된 사용자 !== 현재 사용자 -> true
     const showButton = loggedInUser && loggedInUser.username !== user.username;
+
     // 현재 로그인된 사용자가 이미 팔로우 중인지
     const following = loggedInUser && loggedInUser.following.find((item) => item.username === user.username);
+
     // 현재 로그인된 사용자가 팔로우 중이면 -> Unfollow, 언팔로우 중이면 Follow
     const text = following ? 'Unfollow' : 'Follow';
+
+    const handleFollow = () => {
+        toggleFollow(user.id, !following);
+    }
 
     return (
         <>
             {showButton && (
                 <Button 
                     text={text}
-                    onClick={()=>{}}
+                    onClick={handleFollow}
                     red={text === 'Unfollow'}
                 />
             )}
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -11,7 +11,10 @@ export default function Button({text, onClick, red}: Props) {
 
     return (
         // text가 Unfollow일 경우 -> red classname 설정
-        <button className={`border-none rounded-md py-3 px-8 text-white font-bold leading-4 ${red ? 'bg-red-500' : 'bg-sky-500'}`}>
+        <button 
+            className={`border-none rounded-md py-3 px-8 text-white font-bold leading-4 ${red ? 'bg-red-500' : 'bg-sky-500'}`}
+            onClick={() => onClick()}
+        >
             {text}
         </button>
     )
