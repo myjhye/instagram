db33466c0354226518c5776ab1ad77948abf8a7a
myjhye <t01087633799@gmail.com>
2023-12-22 22:16:29 +0900

    새 포스트 작성 -> ui

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -5,6 +5,7 @@ import PostUserAvatar from './PostUserAvatar';
 import FilesIcon from './ui/icons/FilesIcon';
 import Button from "./ui/Button";
 import { useState } from "react";
+import Image from "next/image";
 
 type Props = {
     user: AuthUser;
@@ -51,12 +52,12 @@ export default function NewPost({user}: Props) {
 
 
     return (
-        <section>
+        <section className="w-full max-w-xl flex flex-col items-center mt-6">
             <PostUserAvatar
                 image={user.image ?? ''} 
                 username={user.username}
             />
-            <form>
+            <form className="w-full flex flex-col mt-2">
                 <input
                     className="hidden" 
                     name='input'
@@ -65,17 +66,39 @@ export default function NewPost({user}: Props) {
                     accept='image/*'
                     onChange={handleChange}
                 />
-                <label 
+                <label
+                    className={`w-full h-60 flex flex-col items-center justify-center ${!file && 'border-2 border-sky-500 border-dashed'}`} 
                     htmlFor="input-upload"
                     onDragEnter={handleDrag}
                     onDragLeave={handleDrag}
                     onDragOver={handleDragOver}
                     onDrop={handleDrop}
                 >
-                    <FilesIcon />
-                    <p>사진을 끌어다 놓거나 클릭하세요</p>
+                    {dragging && (
+                        <div className="absolute inset-0 z-10 bg-sky-500/20 pointer-events-none">
+
+                        </div>
+                    )}
+                    {!file && (
+                        <div className="flex flex-col items-center pointer-events-none">
+                            <FilesIcon />
+                            <p>사진을 끌어다 놓거나 클릭하세요</p>
+                        </div>
+                    )}
+                    {file && (
+                        <div className="relative w-full aspect-square">
+                            <Image
+                                className="object-cover" 
+                                src={URL.createObjectURL(file)}
+                                alt='local file'
+                                fill
+                                sizes='650px'
+                            />
+                        </div>
+                    )}
                 </label>
-                <textarea 
+                <textarea
+                    className="outline-none text-lg border border-neutral-300" 
                     name='text' 
                     id='input-text' 
                     required rows={10} 
