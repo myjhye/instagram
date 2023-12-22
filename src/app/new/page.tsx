2d0849adbddc3eee90c4c5eee8a163fc9af716a8
myjhye <t01087633799@gmail.com>
2023-12-22 22:03:31 +0900

    새 포스트 작성 -> 드래깅

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,5 +1,23 @@
-export default function NewPostPage() {
+import NewPost from "@/components/NewPost";
+import { Metadata } from "next"
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+
+export const metadata: Metadata = {
+    title: '새 게시물 만들기',
+    description: '새 게시물 만들기'
+};
+
+export default async function NewPostPage() {
+    
+    const session = await getServerSession(authOptions);
+    // ?.user -> session에 user가 있는지 검사 -> 항상 user를 전달하려고
+    if (!session?.user) {
+        redirect('/auth/signin');
+    }
+    
     return (
-        <p>new post page</p>
+        <NewPost user={session.user} />
     )
 }
---
+++ src/components/ui/icons/FilesIcon.tsx
@@ -0,0 +1,6 @@
+import { FaPhotoVideo } from "react-icons/fa";
+
+export default function fileIcon() {
+
+    return <FaPhotoVideo classname='w-20 h-20 text-gray-300' />
+}
