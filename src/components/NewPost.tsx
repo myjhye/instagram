fa9f8c4bc62166f3c77a97e07b571ce75e0728f3
myjhye <t01087633799@gmail.com>
2023-12-23 11:22:41 +0900

    새 포스트 작성 -> 클라이언트

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -4,8 +4,10 @@ import { AuthUser } from "@/model/user";
 import PostUserAvatar from './PostUserAvatar';
 import FilesIcon from './ui/icons/FilesIcon';
 import Button from "./ui/Button";
-import { useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import GridSpinner from "./ui/GridSpinner";
 
 type Props = {
     user: AuthUser;
@@ -15,6 +17,10 @@ export default function NewPost({user}: Props) {
 
     const [dragging, setDragging] = useState(false);
     const [file, setFile] = useState<File>();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string>();
+    const textRef = useRef<HTMLTextAreaElement>(null);
+    const router = useRouter();
     
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault();
@@ -22,7 +28,6 @@ export default function NewPost({user}: Props) {
         const files = e.target?.files;
         if (files && files[0]) {
             setFile(files[0]);
-            console.log(files[0]);
         }
     }
 
@@ -48,16 +53,59 @@ export default function NewPost({user}: Props) {
             setFile(files[0]);
             console.log(files[0]);
         }
-    }
+    };
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        
+        if(!file) {
+            return;
+        };
+
+        setLoading(true);
+
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('text', textRef.current?.value ?? '');
+
+        fetch('/api/posts/', {
+            method: 'POST',
+            body: formData
+        })
+        .then(res => {
+            if(!res.ok) {
+                setError(`${res.status} ${res.statusText}`);
+                return;
+            }
+            router.push('/');
+        })
+        .catch(err => setError(err.toString()))
+        .finally(() => setLoading(false));
+    };
 
 
     return (
         <section className="w-full max-w-xl flex flex-col items-center mt-6">
+            {loading && (
+                <div className="absolute inset-0 z-20 text-center pt-[30%] bg-sky-500/20">
+                    <GridSpinner />
+                </div>
+            )}
+            {
+                error && (
+                    <p className="w-full bg-red-100 text-red-600 text-center p-4 mb-4 font-bold">
+                        {error}
+                    </p>
+                )
+            }
             <PostUserAvatar
                 image={user.image ?? ''} 
                 username={user.username}
             />
-            <form className="w-full flex flex-col mt-2">
+            <form 
+                className="w-full flex flex-col mt-2"
+                onSubmit={handleSubmit}
+            >
                 <input
                     className="hidden" 
                     name='input'
@@ -102,7 +150,8 @@ export default function NewPost({user}: Props) {
                     name='text' 
                     id='input-text' 
                     required rows={10} 
-                    placeholder={'문구를 입력하세요...'} 
+                    placeholder={'문구를 입력하세요...'}
+                    ref={textRef} 
                 />
                 <Button 
                     text='공유하기'
