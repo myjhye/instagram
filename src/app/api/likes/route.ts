b20331bb4286760b90c8477f10da122b9346998a
myjhye <t01087633799@gmail.com>
2024-01-03 06:37:54 +0900

    주석 설명 -> 좋아요 라우트, 훅

--- src/app/api/likes/route.ts
+++ src/app/api/likes/route.ts
@@ -2,11 +2,22 @@ import { NextRequest, NextResponse } from "next/server";
 import { dislikePost, likePost } from "@/service/posts";
 import { withSessionUser } from "@/util/session";
 
+/*
+const {id, like} = await req.json();
+1. id : 좋아요를 누른 사용자의 id
+2. like : 좋아요를 누른 게시물
+
+.then(res => NextResponse.json(res))
+- 좋아요 또는 좋아요 취소 결과를 json 형식으로 클라이언트에게 반환 : 클라이언트에서 이를 활용
+** res
+- 좋아요 또는 좋아요 취소 작업이 성공적으로 완료된 후의 결과
+*/
+
 export async function PUT(req: NextRequest) {
     
     return withSessionUser(async (user) => {
 
-        // 요청에서 id, like 값 추출
+        // 요청 (클라이언트)에서 id, like 값 추출
         const {id, like} = await req.json();
 
         // id, like 값이 유효하지 않으면 bad request 응답 생성
@@ -18,7 +29,7 @@ export async function PUT(req: NextRequest) {
         const request = like ? likePost : dislikePost;
 
         // likePost 또는 dislikePost 함수 실행하고 응답 반환
-        return request(id, user.id)
+        return request(id, user.id) 
             // 처리된 결과를 json 형식으로 응답
             .then(res => NextResponse.json(res))
             .catch(error => new Response(JSON.stringify(error), {status: 500}))
--- src/hooks/posts.ts
+++ src/hooks/posts.ts
@@ -2,6 +2,16 @@ import { Comment, SimplePost } from '@/model/post';
 import { useCallback } from 'react';
 import useSWR from 'swr';
 
+/*
+updateLike 함수
+- 클라이언트에서 서버에 요청 보내는 역할 : 좋아요 추가/제거 요청
+- 서버에 데이터를 업데이트 하고 업데이트 된 결과 받아옴
+
+body: JSON.stringify({ id, like })
+- id : 좋아요를 추가/제거할 게시물의 고유 식별자
+- like : true/false 값을 담아서, 좋아요를 추가할 것인지 제거할 것인지 서버에 알려준다. true : 좋아요 추가, false : 좋아요 제거
+*/
+
 async function updateLike(id: string, like: boolean) {
     return fetch('/api/likes', {
         method: 'PUT',
@@ -16,15 +26,50 @@ async function addComment(id: string, comment: string) {
     }).then(res => res.json());
 }
 
+
+/*
+usePosts 커스텀 훅
+- posts 관련 작업 관리
+- updateLike 함수 같은 데이터 업데이트 함수를 컴포넌트에서 사용할 수 있게 함 : 게시물 좋아요 추가/제거 작업 처리하고, 업데이트된 데이터를 화면에 반영
+- useSWR 라이브러리를 사용해 데이터를 가져오고 관리
+*/
+
 export default function usePosts() {
 
-    // useSWR : 데이터 가져오기 -> posts 배열
+    // 본인과 팔로우 하는 사용자의 게시물 가져오기 : getFollowingPostsOf
     const { 
         data: posts, 
         isLoading, 
         error,
         mutate, 
     } = useSWR<SimplePost[]>('/api/posts');
+
+
+
+    /*
+        setLike 
+        - 매개변수 3개 받음
+        1. post : 좋아요 추가/제거 할 게시물 정보
+        2. username : 현재 사용자의 사용자명
+        3. like : true 또는 false 값으로 좋아요 추가/제거 여부
+
+        const newPost = { ... }
+        * like ? [...post.likes, username] 
+        - like 변수가 true 일 시 (사용자가 좋아요 추가 시) : 기존의 likes 배열에 <사용자의 username을 추가>해 새로운 배열 생성 : 사용자가 이미 좋아요를 누른 게시물에 중복으로 좋아요를 누르지 않게 됨
+        * : post.likes.filter(item => item !== username)
+        - like 변수가 false 일 시 (사용자가 좋아요 취소 시) : 기존 likes 배열에서 <사용자의 username을 제외>한 배열 생성 : 사용자가 이미 좋아요를 누른 게시물에서 좋아요 취소 됨
+
+        const newPosts = posts?.map(p => (p.id === post.id ? newPost : p));
+        - 업데이트된 게시물만 변경되고, 나머지 게시물은 변경되지 않은 상태로 남음
+
+        return mutate(updateLike(post.id, like), { ... }
+        - mutate 함수 사용해 updateLike(post.id, like) 함수 호출 : 서버에 좋아요 업데이트 요청 보내기
+
+        optimisticData: newPosts
+        - 서버 응답을 기다리는 동안 화면에 최적화된 데이터 표시
+        * newPosts
+        - 업데이트된 게시물 목록 : 좋아요 즉시 반영
+    */
 
 
     // setLike : 게시물 좋아요 추가/제거
