4dc1976c7ae379f60dff725f2737734000e0c39f
myjhye <t01087633799@gmail.com>
2024-01-05 19:26:44 +0900

    팔로우 목록 스타일링

--- src/app/followings/[username]/page.tsx
+++ src/app/followings/[username]/page.tsx
@@ -2,6 +2,7 @@
 
 import useSWR from 'swr';
 import { FollowingAndFollowers } from '@/model/user';
+import UserCard from '@/components/UserCard';
 
 type Props = {
   params: {
@@ -11,10 +12,13 @@ type Props = {
 
 export default function FollowingsPage({ params: { username } }: Props) {
   
-    const { 
-        data: following, 
-        error 
-    } = useSWR<FollowingAndFollowers>(`/api/followings/${username}`);
+  const { 
+      data: following, 
+      error 
+  } = useSWR<FollowingAndFollowers>(`/api/followings/${username}`);
+
+  console.log(following);
+
 
   if (error) {
     return <div>에러 발생: {error.message}</div>;
@@ -24,11 +28,14 @@ export default function FollowingsPage({ params: { username } }: Props) {
     return <div>Loading...</div>;
   }
 
+  //  map으로 데이터 조회 시 데이터 없는 경우 추가 필요
   return (
     <div>
       {Array.isArray(following) && following.length > 0 ? (
         following.map((user, index) => (
-          <div key={index}>{user.username}</div>
+          <div key={user.username}>
+            <UserCard user={user} />
+          </div>
         ))
       ) : (
         <div>팔로잉이 없습니다.</div>
